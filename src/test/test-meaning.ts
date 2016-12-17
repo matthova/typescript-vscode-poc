ed0ebf27bb94c78e025d3316c572516cdf85bd46
matthova <matthova87@gmail.com>
2016-12-17 15:35:07 -0800

    working when using tsc instead of gulp typescript

--- src/test/test-meaning.ts
+++ src/test/test-meaning.ts
@@ -5,15 +5,17 @@ import { meaning } from '../meaning';
 
 describe('The meaning of life', function() {
 
-  it('should have the correct answer', function() {
+  it('should have the correct answer', function(done) {
     const answer = meaning.answer();
     const expectedAnswer = 42;
     assert.equal(answer, expectedAnswer);
+    done();
   });
 
-  it('should have the correct question', function() {
+  it('should have the correct question', function(done) {
     const question = meaning.question();
     const expectedQuestion = "Sorry, Earth was destroyed before I was able to calculate the question.";
     assert.equal(question, expectedQuestion);
+    done();
   });
 });
